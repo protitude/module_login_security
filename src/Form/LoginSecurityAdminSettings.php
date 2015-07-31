154b6812e2a4580e2194b0dbfa9911f3d26e207e
martin107 <martin107@2659379.no-reply.drupal.org>
2015-07-31 10:59:11 -0300

    Issue #2510768 by martin107: Modernise string translation in forms

--- src/Form/LoginSecurityAdminSettings.php
+++ src/Form/LoginSecurityAdminSettings.php
@@ -39,89 +39,89 @@ class LoginSecurityAdminSettings extends ConfigFormBase {
     $form['track_time'] = array(
       '#type' => 'number',
       '#min' => 0,
-      '#title' => t('Track time'),
+      '#title' => $this->t('Track time'),
       '#default_value' => $config->get('track_time'),
       '#size' => 3,
-      '#description' => t('The time window to check for security violations: the time in hours the login information is kept to compute the login attempts count. A common example could be 24 hours. After that time, the attempt is deleted from the list, and will never be considered again.'),
-      '#field_suffix' => t('Hours'),
+      '#description' => $this->t('The time window to check for security violations: the time in hours the login information is kept to compute the login attempts count. A common example could be 24 hours. After that time, the attempt is deleted from the list, and will never be considered again.'),
+      '#field_suffix' => $this->t('Hours'),
     );
     $form['user_wrong_count'] = array(
       '#type' => 'number',
       '#min' => 0,
-      '#title' => t('Maximum number of login failures before blocking a user'),
+      '#title' => $this->t('Maximum number of login failures before blocking a user'),
       '#default_value' => $config->get('user_wrong_count'),
       '#size' => 3,
-      '#description' => t('Enter the number of login failures a user is allowed. After this amount is reached, the user will be blocked, no matter the host attempting to log in. Use this option carefully on public sites, as an attacker may block your site users. The user blocking protection will not disappear and should be removed manually from the <a href="!user">user management</a> interface.', array('!user' => '/admin/people')),
-      '#field_suffix' => t('Failed attempts'),
+      '#description' => $this->t('Enter the number of login failures a user is allowed. After this amount is reached, the user will be blocked, no matter the host attempting to log in. Use this option carefully on public sites, as an attacker may block your site users. The user blocking protection will not disappear and should be removed manually from the <a href="!user">user management</a> interface.', array('!user' => '/admin/people')),
+      '#field_suffix' => $this->t('Failed attempts'),
     );
     $form['host_wrong_count'] = array(
       '#type' => 'number',
       '#min' => 0,
-      '#title' => t('Maximum number of login failures before soft blocking a host'),
+      '#title' => $this->t('Maximum number of login failures before soft blocking a host'),
       '#default_value' => $config->get('host_wrong_count'),
       '#size' => 3,
-      '#description' => t('Enter the number of login failures a host is allowed. After this amount is reached, the host will not be able to submit the log in form again, but can still browse the site contents as an anonymous user. This protection is effective during the time indicated at tracking time option.'),
-      '#field_suffix' => t('Failed attempts'),
+      '#description' => $this->t('Enter the number of login failures a host is allowed. After this amount is reached, the host will not be able to submit the log in form again, but can still browse the site contents as an anonymous user. This protection is effective during the time indicated at tracking time option.'),
+      '#field_suffix' => $this->t('Failed attempts'),
     );
     $form['host_wrong_count_hard'] = array(
       '#type' => 'number',
       '#min' => 0,
-      '#title' => t('Maximum number of login failures before blocking a host'),
+      '#title' => $this->t('Maximum number of login failures before blocking a host'),
       '#default_value' => $config->get('host_wrong_count_hard'),
       '#size' => 3,
-      '#description' => t('Enter the number of login failures a host is allowed. After this number is reached, the host will be blocked, no matter the username attempting to log in. The host blocking protection will not disappear automatically and should be removed manually from the <a href="!access">access rules</a> administration interface.', array('!access' => '/admin/config/people/ip-blocking')),
-      '#field_suffix' => t('Failed attempts'),
+      '#description' => $this->t('Enter the number of login failures a host is allowed. After this number is reached, the host will be blocked, no matter the username attempting to log in. The host blocking protection will not disappear automatically and should be removed manually from the <a href="!access">access rules</a> administration interface.', array('!access' => '/admin/config/people/ip-blocking')),
+      '#field_suffix' => $this->t('Failed attempts'),
     );
     $form['activity_threshold'] = array(
       '#type' => 'number',
       '#min' => 0,
-      '#title' => t('Maximum number of login failures before detecting an ongoing attack'),
+      '#title' => $this->t('Maximum number of login failures before detecting an ongoing attack'),
       '#default_value' => $config->get('activity_threshold'),
       '#size' => 3,
-      '#description' => t('Enter the number of login failures before creating a warning log entry about this suspicious activity. If the number of invalid login events currently being tracked reach this number, and ongoing attack is detected.'),
-      '#field_suffix' => t('Failed attempts'),
+      '#description' => $this->t('Enter the number of login failures before creating a warning log entry about this suspicious activity. If the number of invalid login events currently being tracked reach this number, and ongoing attack is detected.'),
+      '#field_suffix' => $this->t('Failed attempts'),
     );
     $form['login_messages'] = array(
       '#type' => 'fieldset',
-      '#title' => t('Notifications'),
+      '#title' => $this->t('Notifications'),
     );
     $form['login_messages']['disable_core_login_error'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Disable login failure error message'),
-      '#description' => t('Checking this option prevents the display of login error messages. A user attempting to login will not be aware if the account exists, an invalid user name or password has been submitted, or if the account is blocked. The core messages "Sorry, unrecognized username or password. Have you forgotten your password?" and "The username {username} has not been activated or is blocked." are also hidden.'),
+      '#title' => $this->t('Disable login failure error message'),
+      '#description' => $this->t('Checking this option prevents the display of login error messages. A user attempting to login will not be aware if the account exists, an invalid user name or password has been submitted, or if the account is blocked. The core messages "Sorry, unrecognized username or password. Have you forgotten your password?" and "The username {username} has not been activated or is blocked." are also hidden.'),
       '#default_value' => $config->get('disable_core_login_error'),
     );
     $form['login_messages']['notice_attempts_available'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Notify the user about the number of remaining login attempts'),
+      '#title' => $this->t('Notify the user about the number of remaining login attempts'),
       '#default_value' => $config->get('notice_attempts_available'),
-      '#description' => t('Checking this option, the user is notified about the number of remaining login attempts before the account gets blocked. Security tip: If you enable this option, try to not disclose as much of your login policies as possible in the message shown on any failed login attempt.'),
+      '#description' => $this->t('Checking this option, the user is notified about the number of remaining login attempts before the account gets blocked. Security tip: If you enable this option, try to not disclose as much of your login policies as possible in the message shown on any failed login attempt.'),
     );
     $form['login_messages']['last_login_timestamp'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Display last login timestamp'),
-      '#description' => t('Checking this option, when a user successfully logs in, a message will display the last time he logged into the site.'),
+      '#title' => $this->t('Display last login timestamp'),
+      '#description' => $this->t('Checking this option, when a user successfully logs in, a message will display the last time he logged into the site.'),
       '#default_value' => $config->get('last_login_timestamp'),
     );
     $form['login_messages']['last_access_timestamp'] = array(
       '#type' => 'checkbox',
-      '#title' => t('Display last access timestamp'),
-      '#description' => t('Checking this option, when a user successfully logs in, a message will display the last site access with this account.'),
+      '#title' => $this->t('Display last access timestamp'),
+      '#description' => $this->t('Checking this option, when a user successfully logs in, a message will display the last site access with this account.'),
       '#default_value' => $config->get('last_access_timestamp'),
     );
 
     $form['login_messages']['user_blocked_email_user'] = array(
       '#type' => 'textfield',
-      '#title' => t('Select who should get an email message when a user is blocked by this module'),
-      '#description' => t('No notification will be sent if the field is blank'),
+      '#title' => $this->t('Select who should get an email message when a user is blocked by this module'),
+      '#description' => $this->t('No notification will be sent if the field is blank'),
       '#default_value' => $config->get('user_blocked_email_user'),
       '#autocomplete_path' => 'user/autocomplete',
       '#element_validate' => array(array(get_class($this), 'validUser')),
     );
     $form['login_messages']['login_activity_email_user'] = array(
       '#type' => 'textfield',
-      '#title' => t('Select who should get an email message when an ongoing attack is detected'),
-      '#description' => t('No notification will be sent if the field is blank'),
+      '#title' => $this->t('Select who should get an email message when an ongoing attack is detected'),
+      '#description' => $this->t('No notification will be sent if the field is blank'),
       '#default_value' => $config->get('login_activity_email_user'),
       '#autocomplete_path' => 'user/autocomplete',
       '#element_validate' => array(array(get_class($this), 'validUser')),
@@ -129,7 +129,7 @@ class LoginSecurityAdminSettings extends ConfigFormBase {
 
     $form['login_security']['Notifications'] = array(
       '#type' => 'fieldset',
-      '#title' => t('Edit notification texts'),
+      '#title' => $this->t('Edit notification texts'),
       '#weight' => 3,
       '#collapsible' => TRUE,
       '#collapsed' => TRUE,
@@ -137,73 +137,73 @@ class LoginSecurityAdminSettings extends ConfigFormBase {
     );
     $form['login_security']['Notifications']['notice_attempts_message'] = array(
       '#type' => 'textarea',
-      '#title' => t('Message to be shown on each failed login attempt'),
+      '#title' => $this->t('Message to be shown on each failed login attempt'),
       '#rows' => 2,
       '#default_value' => $config->get('notice_attempts_message'),
-      '#description' => t('Enter the message string to be shown if the login fails after the form is submitted. You can use any of the placeholders here.'),
+      '#description' => $this->t('Enter the message string to be shown if the login fails after the form is submitted. You can use any of the placeholders here.'),
     );
     $form['login_security']['Notifications']['host_soft_banned'] = array(
       '#type' => 'textarea',
-      '#title' => t('Message for banned host (Soft IP ban)'),
+      '#title' => $this->t('Message for banned host (Soft IP ban)'),
       '#rows' => 2,
       '#default_value' => $config->get('host_soft_banned'),
-      '#description' => t('Enter the soft IP ban message to be shown when a host attempts to log in too many times.'),
+      '#description' => $this->t('Enter the soft IP ban message to be shown when a host attempts to log in too many times.'),
     );
     $form['login_security']['Notifications']['host_hard_banned'] = array(
       '#type' => 'textarea',
       '#rows' => 2,
-      '#title' => t('Message for banned host (Hard IP ban)'),
+      '#title' => $this->t('Message for banned host (Hard IP ban)'),
       '#default_value' => $config->get('host_hard_banned'),
-      '#description' => t('Enter the hard IP ban message to be shown when a host attempts to log in too many times.'),
+      '#description' => $this->t('Enter the hard IP ban message to be shown when a host attempts to log in too many times.'),
     );
     $form['login_security']['Notifications']['user_blocked'] = array(
       '#type' => 'textarea',
       '#rows' => 2,
-      '#title' => t('Message when user is blocked by uid'),
+      '#title' => $this->t('Message when user is blocked by uid'),
       '#default_value' => $config->get('user_blocked'),
-      '#description' => t('Enter the message to be shown when a user gets blocked due to enough failed login attempts.'),
+      '#description' => $this->t('Enter the message to be shown when a user gets blocked due to enough failed login attempts.'),
     );
 
     $form['login_security']['Notifications']['user_block_email'] = array(
       '#type' => 'fieldset',
-      '#title' => t('Email to be sent to the defined user for blocked accounts.'),
+      '#title' => $this->t('Email to be sent to the defined user for blocked accounts.'),
       '#weight' => 3,
-      '#description' => t('Configure the subject and body of the email message.'),
+      '#description' => $this->t('Configure the subject and body of the email message.'),
     );
     $form['login_security']['Notifications']['user_block_email']['user_blocked_email_subject'] = array(
       '#type' => 'textfield',
-      '#title' => t('Email subject'),
+      '#title' => $this->t('Email subject'),
       '#default_value' => $config->get('user_blocked_email_subject'),
     );
     $form['login_security']['Notifications']['user_block_email']['user_blocked_email_body'] = array(
       '#type' => 'textarea',
-      '#title' => t('Email body'),
+      '#title' => $this->t('Email body'),
       '#default_value' => $config->get('user_blocked_email_body'),
-      '#description' => t('Enter the message to be sent to the administrator informing a user has been blocked.'),
+      '#description' => $this->t('Enter the message to be sent to the administrator informing a user has been blocked.'),
     );
 
     $form['login_security']['Notifications']['login_activity_email'] = array(
       '#type' => 'fieldset',
-      '#title' => t('Email to be sent to the defined user for ongoing attack detections.'),
+      '#title' => $this->t('Email to be sent to the defined user for ongoing attack detections.'),
       '#weight' => 3,
-      '#description' => t('Configure the subject and body of the email message.'),
+      '#description' => $this->t('Configure the subject and body of the email message.'),
     );
     $form['login_security']['Notifications']['login_activity_email']['login_activity_email_subject'] = array(
       '#type' => 'textfield',
-      '#title' => t('Email subject'),
+      '#title' => $this->t('Email subject'),
       '#default_value' => $config->get('login_activity_email_subject'),
     );
     $form['login_security']['Notifications']['login_activity_email']['login_activity_email_body'] = array(
       '#type' => 'textarea',
-      '#title' => t('Email body'),
+      '#title' => $this->t('Email body'),
       '#default_value' => $config->get('login_activity_email_body'),
-      '#description' => t('Enter the message to be sent to the administrator informing about supicious activity.'),
+      '#description' => $this->t('Enter the message to be sent to the administrator informing about supicious activity.'),
     );
 
     // Clean event tracking list.
     $form['buttons']['clean_tracked_events'] = array(
       '#type' => 'submit',
-      '#value' => t('Clear event tracking information'),
+      '#value' => $this->t('Clear event tracking information'),
       '#weight' => 20,
       '#submit' => array('_login_security_clean_tracked_events'),
     );
@@ -221,7 +221,7 @@ class LoginSecurityAdminSettings extends ConfigFormBase {
       ->execute()
       ->fetchField();
       if (intval($count) != 1) {
-        $form_state->setError($element, t('The @field field should be a valid username.', array('@field' => $element['#title'])));
+        $form_state->setError($element, $this->t('The @field field should be a valid username.', array('@field' => $element['#title'])));
       }
     }
   }
